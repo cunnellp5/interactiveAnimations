0bbf8d5a71bdef082822d37018de4bd51b0800ac
cunnellp5 <philip.cunnell@colorado.edu>
2018-11-21 10:57:37 -0500

    js to switch slides with guards to prevent otehr devs from messing up my flow

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('hello from js file')
+window.addEventListener('DOMContentLoaded', function (e) {
+  document.querySelector('hp-presentation').onclick = handlePresentationClick;
+})
+
+function handlePresentationClick(e){
+  var current = document.querySelector('hp-slide.active');
+  var next = current.nextElementSibling;
+
+  while (next && next.tagName != 'HP-SLIDE') {
+    next = next.nextElementSibling;
+  }
+
+  if (next) {
+    current.classList.remove('active');
+    next.classList.add('active');
+  }
+}
